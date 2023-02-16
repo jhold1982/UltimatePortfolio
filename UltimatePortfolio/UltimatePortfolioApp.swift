ddb8378c59731f0951e05e6b445919b28110750c
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-02-15 18:17:53 -0600

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Justin Hold on 2/15/23.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
