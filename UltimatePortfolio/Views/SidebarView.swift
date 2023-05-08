97e373ff6efaa7c6005c6b1d6c7b89ee1221dc16
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2023-05-08 13:19:27 -0500

    Added localization

--- UltimatePortfolio/Views/SidebarView.swift
+++ UltimatePortfolio/Views/SidebarView.swift
@@ -33,7 +33,7 @@ struct SidebarView: View {
 			Section("Smart Filters") {
 				ForEach(smartFilters) { filter in
 					NavigationLink(value: filter) {
-						Label(filter.name, systemImage: filter.icon)
+						Label(LocalizedStringKey(filter.name), systemImage: filter.icon)
 					}
 				}
 			}
