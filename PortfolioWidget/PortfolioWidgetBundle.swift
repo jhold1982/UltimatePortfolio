d2eb96aa262015f48c1f88b5de486a2ec638854a
Justin Hold <36420615+jhold1982@users.noreply.github.com>
2024-09-08 17:19:14 -0500

    Started adding Widget

---
+++ PortfolioWidget/PortfolioWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  PortfolioWidgetBundle.swift
+//  PortfolioWidget
+//
+//  Created by Justin Hold on 9/5/24.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct PortfolioWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        PortfolioWidget()
+    }
+}
--- UltimatePortfolio/DataModels/DataController.swift
+++ UltimatePortfolio/DataModels/DataController.swift
@@ -333,37 +333,6 @@ class DataController: ObservableObject {
 	func count<T>(for fetchRequest: NSFetchRequest<T>) -> Int {
 		(try? container.viewContext.count(for: fetchRequest)) ?? 0
 	}
-
-	func hasEarned(award: Award) -> Bool {
-		switch award.criterion {
-		case "issues":
-			// return true if they added a certain number of issues
-			let fetchRequest = Issue.fetchRequest()
-			let awardCount = count(for: fetchRequest)
-			return awardCount >= award.value
-
-		case "closed":
-			// return true if they closed a certain number of issues
-			let fetchRequest = Issue.fetchRequest()
-			fetchRequest.predicate = NSPredicate(format: "completed = true")
-			let awardCount = count(for: fetchRequest)
-			return awardCount >= award.value
-
-		case "tags":
-			// return true if they created a certain number of tags
-			let fetchRequest = Tag.fetchRequest()
-			let awardCount = count(for: fetchRequest)
-			return awardCount >= award.value
-			
-		case "unlock":
-			return fullVersionUnlocked
-
-		default:
-			// an unknown award criterion; this should never be allowed
-			// fatalError("Unknown award criterion: \(award.criterion)")
-			return false
-		}
-	}
 	
 	func issue(with uniqueIdentifier: String) -> Issue? {
 		guard let url = URL(string: uniqueIdentifier) else {
